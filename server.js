325f2460c72ea192d090b4b7f36fc2d10bb40d62
Nigel D. Watkins <nigeldwatkins@gmail.com>
2023-07-14 22:59:34 -0400

    server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+// Importing the required dependencies 
+const express = require("express");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const exphb = require("express-handlebars");
